16cac41393d1ea849bb4caf7dd5d1d8231983657
pablo-sa10 <pablomoura2016@gmail.com>
2024-08-30 09:01:17 -0300

    atividade da semana 3 feita tbm

---
+++ semana3/js/ex1.js
@@ -0,0 +1,24 @@
+let lista = []
+    let btn = document.getElementById("btn")
+    let ul = document.getElementById("lista") 
+    
+    btn.onclick = () => {
+        let texto = document.getElementById("item").value;
+        
+        if(texto == ""){
+            alert("digite algo para adicionar a lista")
+            return
+        }
+
+        lista.push(texto)
+        lista.sort()
+
+        ul.innerHTML = "";
+
+        lista.forEach(element => {
+            let li = document.createElement("li")
+            li.textContent = element;
+
+            ul.appendChild(li);
+        });
+    }
