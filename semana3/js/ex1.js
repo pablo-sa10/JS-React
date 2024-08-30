0f8ab7dda925d4bd0cf576632453f82d457dbb6a
pablo-sa10 <pablomoura2016@gmail.com>
2024-08-30 09:06:24 -0300

    .

--- semana3/js/ex1.js
+++ semana3/js/ex1.js
@@ -1,24 +1,24 @@
 let lista = []
-    let btn = document.getElementById("btn")
-    let ul = document.getElementById("lista") 
-    
-    btn.onclick = () => {
-        let texto = document.getElementById("item").value;
-        
-        if(texto == ""){
-            alert("digite algo para adicionar a lista")
-            return
-        }
+let btn = document.getElementById("btn")
+let ul = document.getElementById("lista")
 
-        lista.push(texto)
-        lista.sort()
+btn.onclick = () => {
+    let texto = document.getElementById("item").value;
 
-        ul.innerHTML = "";
-
-        lista.forEach(element => {
-            let li = document.createElement("li")
-            li.textContent = element;
-
-            ul.append(li);
-        });
+    if (texto == "") {
+        alert("digite algo para adicionar a lista")
+        return
     }
+
+    lista.push(texto)
+    lista.sort()
+
+    ul.innerHTML = "";
+
+    lista.forEach(element => {
+        let li = document.createElement("li")
+        li.textContent = element;
+
+        ul.append(li);
+    });
+}
